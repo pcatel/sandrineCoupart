b40821715e5a265dabec4a73af1f619465fd117a
pcatel <pcatel@pascalcatel.com>
2023-07-19 10:30:29 +0200

    ameliration

--- php/navbar.php
+++ php/navbar.php
@@ -1,38 +1,41 @@
 <!-- Navbar Start -->
-<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-0 pe-5">
-        <a href="index.html" class="navbar-brand ps-5 me-0">
-            <h4 class="text-white m-0 SiteName">Sandrine Coupart : Diététicienne - Nutritionniste</h4>
-           
-        </a>
+<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-0 pe-1">
+    <a href="index.html" class="navbar-brand ps-1 me-0">
+        <h4 class="text-white m-0 SiteName">Sandrine Coupart : Diététicienne - Nutritionniste</h4>
+
+    </a>
 
 
-        <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index.php" class="nav-item nav-link active">Accueil</a>
-                <a href="aPropos.php" class="nav-item nav-link">Qui suis-je ?</a>
-                 
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Mes services</a>
-                    <div class="dropdown-menu bg-light m-0">
-                        <a href="consultations.php" class="dropdown-item">Consultations</a>
-                        <a href="ateliersPrevention.php" class="dropdown-item">Ateliers prévention</a>
-                        <a href="infosNutrition.php" class="dropdown-item">Infos nutrition</a>
-                        <a href="temoignages.php" class="dropdown-item">Témoignages</a>
-                        <a href="recettes.php" class="dropdown-item">Recettes</a>
-                        
+    <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
 
-                    </div> 
+            <a href="connexion.php" id="seconnecter" class="nav-item nav-link">Se connecter</a>
+            <?php include 'updateconnecter.php'; ?>
+
+            <a href="index.php" class="nav-item nav-link active">Accueil</a>
+            <a href="aPropos.php" class="nav-item nav-link">Qui suis-je ?</a>
+
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Mes services</a>
+                <div class="dropdown-menu bg-light m-0">
+                    <a href="consultations.php" class="dropdown-item">Consultations</a>
+                    <a href="ateliersPrevention.php" class="dropdown-item">Ateliers prévention</a>
+                    <a href="infosNutrition.php" class="dropdown-item">Infos nutrition</a>
+                    <a href="temoignages.php" class="dropdown-item">Témoignages</a>
+                    <a href="recettes.php" class="dropdown-item">Recettes</a>
+
+
                 </div>
-        
-                <a href="contact.php" class="nav-item nav-link">Contact</a>
-              
-               
             </div>
-            <a href="connexion.php" id= "seconnecter" class="btn btn-primary px-3 d-none d-lg-block">Se connecter</a>
-            <?php include 'updateconnecter.php'; ?>    
+
+            <a href="contact.php" class="nav-item nav-link">Contact</a>
+
+
         </div>
-    </nav>
-    <!-- Navbar End -->
+
+    </div>
+</nav>
+<!-- Navbar End -->
--- php/updateconnecter.php
+++ php/updateconnecter.php
@@ -5,10 +5,18 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) {
     $email = $_SESSION['email'];
     
     
+    
     if ($_SESSION['typeUser'] === 'admin') {
-        echo '<script>document.getElementById("seconnecter").innerHTML = \'<a href="admin.php" class="btn btn-primary px-3 d-none d-lg-block">' . $email . '</a>\';</script>';
+        echo '<script>document.getElementById("seconnecter").innerHTML = \'<a href="admin.php" class="nav-item nav-link">' . $email . '</a>\';</script>';
+       
+
+
+
+       
     } else {
-        echo '<script>document.getElementById("seconnecter").innerHTML = \'<a href="compteUser.php" class="btn btn-primary px-3 d-none d-lg-block">' . $email . '</a>\';</script>';
+        echo '<script>document.getElementById("seconnecter").innerHTML = \'<a href="compteUser.php" class="nav-item nav-link">' . $email . '</a>\';</script>';
+   
+
     }
 }
 ?>
